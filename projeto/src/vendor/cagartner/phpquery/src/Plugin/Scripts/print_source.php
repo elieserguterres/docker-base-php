dce7f8f2414d0d53fe6fee594689cb349e86206c
Eli Guterres <elieserguterres@gmail.com>
2021-02-24 16:07:20 -0300

    First Commit

--- projeto/src/vendor/cagartner/phpquery/src/Plugin/Scripts/print_source.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Script outputs document markup and changes HTML special chars to entities.
- *
- * @author Tobiasz Cudnik <tobiasz.cudnik/gmail.com>
- */
-/** @var PhpQueryObject */
-$self = $self;
-$return = htmlspecialchars($self);
